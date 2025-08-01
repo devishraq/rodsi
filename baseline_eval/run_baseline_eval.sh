921af5c69df5d1193b3eb827a5b5a7442edbd2b2
devishraq <devishraq@gmail.com>
2025-08-02 00:41:28 +0600

    added model_name parameter specifically for qwen/qwen3-1.7B as it doesn't support pretrained parameter, so removed that

--- baseline_eval/run_baseline_eval.sh
+++ baseline_eval/run_baseline_eval.sh
@@ -30,19 +30,21 @@ TASK_LIST=(
   "minerva"
 )
 
-
-
 echo "-- Starting Eval loop --"
 
 for MODEL in "${MODEL_LIST[@]}"; do
 
+  if [[ $MODEL == "Qwen/Qwen3-1.7B" ]]; then
+MODEL_ARGS="model_name=$MODEL,dtype=bfloat16,data_parallel_size=1,max_model_length=32768,gpu_memory_utilization=0.8,generation_parameters={max_new_tokens:32768,temperature:0.6,top_p:0.95}"
+  else
 MODEL_ARGS="pretrained=$MODEL,dtype=bfloat16,data_parallel_size=1,max_model_length=32768,gpu_memory_utilization=0.8,generation_parameters={max_new_tokens:32768,temperature:0.6,top_p:0.95}"
-
+  fi
 
   for TASK in "${TASK_LIST[@]}"; do
     OUTPUT_PATH="${RESULTS_DIR}/${MODEL//\//_}/${TASK}"
 
-    echo "-------------------------\n Currently, Evaluation Running of $MODEL on $TASK \n-------------------------"
+    echo "-------------------------"
+    echo "Currently, Evaluation Running of $MODEL on $TASK"
 
     lighteval vllm $MODEL_ARGS "custom|$TASK|0|0" \
           --custom-tasks $CUSTOM_EVALUATION_SCRIPT \
